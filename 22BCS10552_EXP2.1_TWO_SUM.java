c5f390c568197035488f0eb4c0ad80e7add1f09a
harshkrsingh2 <149362539+harshkrsingh2@users.noreply.github.com>
2025-02-02 10:47:28 +0530

    Create 22BCS10552_EXP2.1_TWO_SUM.java

---
+++ 22BCS10552_EXP2.1_TWO_SUM.java
@@ -0,0 +1,33 @@
+//22BCS10552
+//Harsh Kumar Singh
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; 
+    }
+
+public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println("Output: [" + result1[0] + ", " + result1[1] + "]");
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println("Output: [" + result2[0] + ", " + result2[1] + "]");
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = twoSum(nums3, target3);
+        System.out.println("Output: [" + result3[0] + ", " + result3[1] + "]");
+    }
+}
